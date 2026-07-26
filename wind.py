126552c73c218d1e3cc00f12d2b33f9b725c7d4a
atomixkl <atomixklw@gmail.com>
2026-07-26 21:33:11 +0300

    potuzho2

--- wind.py
+++ wind.py
@@ -3,14 +3,22 @@ import tkinter as tk
 root = tk.Tk()
 root.title("ABc")
 root.geometry("1000x700")
+root.config(bg="#070707")
 
-top_label = tk.Label(root, text = "Гарного дня :3", font=("Arial", 20, "bold"))
+clicks = 0
+
+def launch():
+    global clicks
+    clicks += 1
+    label.config(text=f"Ви успішно запустили ядерку на москву x{clicks} раз(-ів)!", font=("Arial", 16,), fg="white", bg="#070707")
+
+top_label = tk.Label(root, text = "Гарного дня :3", font=("Arial", 20, "bold"), fg="white", bg="#070707")
 top_label.pack(side="top", pady=10)
 
-label = tk.Label(root, text = "ПТХ ПНХ", font=("Arial, 16"))
+label = tk.Label(root, text = "ПТХ ПНХ", font=("Arial, 16"), fg="white", bg="#070707")
 label.pack(pady=(250, 20))
 
-button = tk.Button(root, text = "Запустити ядерку на москву", command = lambda: label.config(text="Ви успішно запустили ядерку на москву", font=("Arial, 16")))
+button = tk.Button(root, text = "Запустити ядерку на москву", bg="red", fg="white", command=launch)
 button.pack(pady=0)
 
 
